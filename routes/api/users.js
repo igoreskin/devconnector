d1a26b6288769916df3d9ce739a7c555bc17e4c2
Igor Eskin <eskinhd@gmail.com>
2019-05-16 17:04:47 -0400

    Small changes along the revised course

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+
+const { check, validationResult } = require('express-validator/check');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
@@ -20,7 +22,18 @@ router.get('/', (req, res) => {
 // @route   POST /api/users
 // @desc    Register user
 // @access  Public
-router.post('/test', (req, res) => res.json({msg: 'Users Works'}));
+router.post('/', [
+  check('name', 'Name is required').not().isEmpty(),
+  check('email', 'Please include a valid email').isEmail(),
+  check('password', 'Please enter a password with 6 or more characters').isLength({min: 6})
+], 
+(req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({errors: errors.array()});
+  }
+  res.send('User route');
+});
 
 // @route   POST /api/users/register
 // @desc    Register user

4f0411320f0e6548c8b8f4391f488f8a343f73c0
Igor Eskin <eskinhd@gmail.com>
2019-05-25 21:44:10 -0400

    Profile route to get user repos from GitHub

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,6 @@
 module.exports = {
   mongoURI: 'mongodb+srv://igoreskin:MongoDB20@cluster0-kbdpm.mongodb.net/devconnector?retryWrites=true',
-  secretOrKey: 'secret'
+  secretOrKey: 'secret',
+  "githubClientId": "da36bee2d8c2fbea1cc6",
+  "githubSecret": "e42daa2d48290435421d3d1e56e16b476bbf7242"
 }
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const request = require('request');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator/check');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const keys = require('../../config/keys');
 
 // @route   GET /api/profile/me
 // @desc    Get current users profile
@@ -170,9 +172,85 @@ router.delete('/experience/:exp_id', auth, async (req, res) => {
     profile.experience.splice(removeIndex, 1);
     await profile.save();
     res.json(profile);
-    
+
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+
+
+// @route   PUT /api/profile/education
+// @desc    Add profile education
+// @access  Private
+router.put('/education', [auth, [
+  check('school', 'School is required').not().isEmpty(),
+  check('degree', 'Degree is required').not().isEmpty(),
+  check('fieldofstudy', 'Field of study is required').not().isEmpty(),
+  check('from', 'From date is required').not().isEmpty()
+]], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array });
+  }
+
+  const { school, degree, fieldofstudy, from, to, current, description } = req.body;
+
+    const newEdu = { school, degree, fieldofstudy, from, to, current, description }
+  // the same as title = title, etc.It creates an object with the data that user submits
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+    profile.education.unshift(newEdu);
+    await profile.save();
+    res.json(profile);
+
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+
+});
+
+// @route   DELETE /api/profile/education/:edu_id
+// @desc    Delete education from profile
+// @access  Private
+router.delete('/education/:edu_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+    // Get remove index 
+    const removeIndex = profile.education.map(item => item.id).indexOf(req.params.edu_id);
+    profile.education.splice(removeIndex, 1);
+    await profile.save();
+    res.json(profile);
+
   } catch (err) {
     onsole.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route   GET /api/profile/github/:username
+// @desc    Get user repos from GitHub
+// @access  Public
+router.get('/github/:username', (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${keys.githubClientId}&client_secret=${keys.githubSecret}`,
+      method: 'GET',
+      headers: { 'user-agent': 'node.js' }
+    };
+    request(options, (error, response, body) => {
+      if(error) console.error(error);
+
+      if(response.statusCode !== 200) {
+        return res.status(404).json({ msg: 'No GitHub profile found' });
+      }
+
+      res.json(JSON.parse(body));
+    })
+  } catch (err) {
+    console.error(err.message);
     res.status(500).send('Server Error');
   }
 })

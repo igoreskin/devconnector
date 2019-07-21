701af586fe7cf48738d262710ebe79b35c33389f
Igor Eskin <eskinhd@gmail.com>
2019-07-21 17:07:31 -0400

    Profile clears on logout; user prompted to create profile

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { useEffect, Fragment } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
@@ -12,7 +13,11 @@ const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, load
   return loading && profile === null ? <Spinner /> : <Fragment>
     <h1 className="large text-primary">Dashboard</h1>
     <p className="lead"><i className="fas fa-user"></i> Welcome { user && user.name }</p> { /* 'user &&' is the same as 'if user' */}
-    {profile !== null ? <Fragment>has</Fragment> : <Fragment>has not</Fragment>}
+    
+    {profile !== null ? <Fragment>has</Fragment> : <Fragment>
+      <p>You have not yet set up a profile, please add some info</p>
+      <Link to='./create-profile' className="btn btn-primary my-1">Create Profile</Link>
+      </Fragment>}
   </Fragment>
 };
 

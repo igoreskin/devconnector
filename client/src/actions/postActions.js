efab7f0c5e85c2d830ffa295a282c9d1fc2140ac
Igor Eskin <eskinhd@gmail.com>
2019-09-05 16:59:08 -0400

    Post delete functionality

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -53,3 +53,22 @@ export const removeLike = postId => async dispatch => {
     });
   }
 };
+
+// Delete post 
+export const deletePost = id => async dispatch => {
+  try {
+    const res = await axios.delete(`/api/posts/unlike/${id}`);
+
+    dispatch({
+      type: DELETE_POST,
+      payload: id
+    });
+
+    dispatch(setAlert('Post removed', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};

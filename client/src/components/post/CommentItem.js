3cc0caaf7bc873c9ae12bd946a5c37f2f7bbe3fa
Igor Eskin <eskinhd@gmail.com>
2019-09-09 16:21:54 -0400

    Comments displayed in the post view

---
+++ client/src/components/post/CommentItem.js
@@ -0,0 +1,42 @@
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Moment from 'react-moment';
+
+const CommentItem = ({ postId, comment: {_id, text, name, avatar, user, date }, auth }) => {
+  return (
+    <div className="post bg-white p-1 my-1">
+      <div>
+        <Link to={`/profile/${user}`}> {/** this is the user's id from comment */}
+          <img
+            className="round-img"
+            src={avatar}
+            alt=""
+          />
+          <h4>{name}</h4>
+        </Link>
+      </div>
+      <div>
+        <p className="my-1">
+          {text}
+        </p>
+          <p className="post-date">
+            Posted <Moment format='YYYY/MM/DD'>{date}</Moment>
+        </p>
+      </div>
+    </div>
+  )
+}
+
+CommentItem.propTypes = {
+  postId: PropTypes.number.isRequired,
+  comment: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired,
+}
+
+const mapSteteToProps = state => ({
+  auth: state.auth
+})
+
+export default connect(mapSteteToProps, { })(CommentItem);
--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import Spinner from '../layout/Spinner';
 import PostItem from '../posts/PostItem';
 import CommentForm from '../post/CommentForm';
+import CommentItem from '../post/CommentItem';
 import { getPost } from '../../actions/postActions';
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
@@ -16,6 +17,12 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
     <Link to='/posts' className='btn'>Back To Posts</Link>
     <PostItem post={post} showActions={false} />
     <CommentForm postId={post._id}/>
+    <div className="comments">
+      {post.comments.map(comment => (
+        <CommentItem key={comment._id} comment={comment} postId={post._id} />
+      )
+      )}
+    </div>
   </Fragment>
 }
 
